3a19f93b89a54cde0253ca66db5d2cf6c46d2137
Kleynhans <Amy.Kleynhans@Emerson.com>
2023-08-13 18:54:50 -0600

    formatted search cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,49 @@ var searchInput = document.querySelector('#searchInput');
 var resultBody = document.querySelector('#searchBody');
 var modalBTNEl = document.querySelector(".modalBTN");
 var modal = document.querySelector('#streaming-modal');
+var searchHistoryContainer = document.querySelector('#searchHistory')
 var addedInfo = ""
+var searchArray = [];
+
+render()
 
 function search(event) {
     event.preventDefault();
 
     var input = searchInput.value.split(' ');
+
+
     for (let i = 0; i < input.length; i++) {
 
         if (i < 0) {
             input[i] = '%20' + input[i];
         }
     }
-     addedInfo = input.toString();
+    addedInfo = input.toString();
     mbdFetch(addedInfo)
+
+    // add to array
+    var historyInput = searchInput.value
+
+    searchArray.unshift(historyInput)
+
+
+    // if array is more than 10 drop last one
+    if (searchArray.length >= 11) {
+        searchArray.pop();
+        searchHistoryContainer.removeChild(searchHistoryContainer.lastChild);
+    }
+
+    // save to local storage and create button for search history
+    localStorage.setItem("searchHistory", JSON.stringify(searchArray))
+
+
+
+    var buttonCreate = document.createElement('button');
+    searchHistoryContainer.prepend(buttonCreate);
+    buttonCreate.setAttribute("data-movie", historyInput);
+    buttonCreate.setAttribute("data-addedInfo", addedInfo);
+    buttonCreate.textContent = historyInput;
 
 }
 
@@ -42,6 +71,10 @@ function mbdFetch(addedInfo) {
             while (resultBody.firstChild) {
                 resultBody.removeChild(resultBody.firstChild);
             }
+            while (modal.firstChild) {
+                modal.removeChild(modal.firstChild);
+            }
+            
             // get results to 10 elements
             if (data.Search.length > 10) {
                 while (data.Search.length > 10) {
@@ -77,8 +110,6 @@ function searchstring(element) {
     var addedInfo = input.toString().replace(/\,/g, '');
     console.log(addedInfo)
 
-    // streamingAvailabilityFetch(addedInfo);
-
 }
 
 // fetch data of titles from streaming API
@@ -104,11 +135,6 @@ function streamingAvailabilityFetch(addedInfo) {
             streamingResults(data)
         })
 
-    // useful data
-    // streaming platform = result[i].streamingInfo.us[i].service
-    // streaming platform = result[i].streamingInfo.us[i].streamingtype
-    // streaming platform = result[i].streamingInfo.us[i].quality
-    // streaming platform = result[i].streamingInfo.us[i].link
 }
 
 // create elements to display data from moviedatabase API
@@ -117,26 +143,21 @@ function createElements(element) {
 
     // create cards for results
     var resultCard = document.createElement('div');
-    resultCard.classList.add('box')
-    resultCard.classList.add('is-flex')
-    resultCard.classList.add('is-flex-direction-column')
+    resultCard.classList.add('box', 'is-flex', 'is-flex-direction-column')
     resultBody.append(resultCard);
 
     // fill card
     var title = document.createElement('h2');
-    title.classList.add('m-2')
-    title.classList.add('has-text-weight-bold')
-    title.classList.add('is-size-5')
+    title.classList.add('m-1')
+    title.classList.add('has-text-weight-bold','is-size-7')
     var year = document.createElement('p');
-    year.classList.add('has-text-weight-bold')
-    year.classList.add('m-2')
+    year.classList.add('cardp', 'm-1')
     var type = document.createElement('p');
-    type.classList.add('has-text-weight-bold')
-    type.classList.add('m-2')
+    type.classList.add('cardp', 'm-1')
     var poster = document.createElement('img');
-    poster.classList.add('m-2')
+    poster.classList.add('m-1', 'poster')
     var modalBTN = document.createElement('button');
-    
+
 
     resultCard.append(title);
     resultCard.append(year);
@@ -160,31 +181,39 @@ function streamingResults(data) {
     var modalContent = document.createElement('div');
     var close = document.createElement('span')
 
-     // append modal elements
+    // append modal elements
     // document.body.append(mymodal);
     modal.append(modalContent);
     modalContent.append(close);
 
     // add classes and ids to elements
     close.classList.add('close');
-     // When the user clicks on <span> (x), close the modal
+    // When the user clicks on <span> (x), close the modal
     close.addEventListener("click", function (e) {
         modal.style.display = "none";
     })
     modalContent.classList.add('modal-content');
-   
-    
 
-     // populate elements
+    // populate elements
     close.textContent = 'x'
 
-     // populate modal with 3 modes of streaming
-    for (let i = 0; i < data.result.length; i++) {
+    // for loop search for results
+    // populate modal with 5 modes of streaming
+    for (let i = 0; i < 6; i++) {
         var serviceName = document.createElement('h2')
+        var streamingType = document.createElement('p')
+        var quality = document.createElement('p')
+        var link = document.createElement('p')
         // append elements
         modalContent.append(serviceName);
-        serviceName.textContent = data.result[i].streamingInfo.us[i].service;
-
+        modalContent.append(streamingType);
+        modalContent.append(quality);
+        modalContent.append(link);
+        // populate elements
+        serviceName.textContent = data.result[0].streamingInfo.us[i].service;
+        streamingType.textContent = data.result[0].streamingInfo.us[i].streamingtype
+        quality.textContent = data.result[0].streamingInfo.us[i].quality
+        link.textContent = data.result[0].streamingInfo.us[i].link
     }
 }
 
@@ -196,14 +225,55 @@ resultBody.addEventListener('click', function (event) {
     } else {
         modal.style.display = "none";
     }
-    
+
 })
 
-// event listener for search button
+
+// pull from local storage
+function render() {
+    searchArray = JSON.parse(localStorage.getItem("searchHistory"));
+
+    if (searchArray !== null) {
+
+
+        searchArray.forEach(movie => {
+
+            var input = movie.split(' ');
+
+            for (let i = 0; i < input.length; i++) {
+
+                if (i < 0) {
+                    input[i] = '%20' + input[i];
+                }
+            }
+            addedInfo = input.toString();
+
+            var buttonCreate = document.createElement('button');
+            buttonCreate.setAttribute("data-movie", movie);
+            buttonCreate.setAttribute("data-addedInfo", addedInfo);
+            buttonCreate.classList.add('m-2')
+            searchHistoryContainer.append(buttonCreate);
+            buttonCreate.textContent = movie;
+        });
+
+    } else {
+        searchArray = [];
+    }
+}
+
+function buttonClickHandler(event) {
+    var moviestring = event.target.getAttribute('data-addedInfo');
+    mbdFetch(moviestring)
+}
+
+
+// event listener for buttons
 searchBTN.addEventListener('click', search)
+searchHistoryContainer.addEventListener('click', buttonClickHandler)
+
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener('click', function (e) {
-  
+
     if (e.target == modal) {
         modal.style.display = "none"
     }
